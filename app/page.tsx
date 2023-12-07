0bb71dd36ca88ad6023190ec233082b6941269da
fnmatheus <nasc.matheusfrancisco@gmail.com>
2023-12-07 14:21:13 -0300

    feat: pdfmake

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button onClick={handleGeneratePdf} className="bg-blue-500 text-white p-1">
-        Download
+        Abrir
       </button>
     </main>
   )
